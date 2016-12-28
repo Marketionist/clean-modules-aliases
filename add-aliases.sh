9144ed7b988257711e1a4f2c624343fc9e9f9877
Marketionist <marketionist@gmail.com>
2016-12-28 21:45:19 +0200

    Added parametrized PROGRAM_NAME to all action messages

--- add-aliases.sh
+++ add-aliases.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROGRAM_NAME="[clean-modules-aliases]"
+
 if [ -f ~/.bashrc ]; then
     echo "alias clean='
     npm cache clean && bower cache clean
@@ -13,25 +15,25 @@ if [ -f ~/.bashrc ]; then
     '" >> ~/.bashrc
 
     source ~/.bashrc
-    echo "=> Added clean and reinst aliases to .bashrc"
+    echo "$PROGRAM_NAME: Added clean and reinst aliases to .bashrc"
 else
     if [ -f ~/.bash_profile ]; then
         echo "alias clean='
         npm cache clean && bower cache clean
         rm -rf node_modules/ build/ components/ generated/ .cache/
-        echo \"=> Cleaned node and bower modules\"
+        echo \"$PROGRAM_NAME: Cleaned node and bower modules\"
         '" >> ~/.bash_profile
 
         echo "alias reinst='
         npm cache clean && bower cache clean
         rm -rf node_modules/ build/ components/ generated/ .cache/
-        echo \"=> Cleaned node and bower modules\"
+        echo \"$PROGRAM_NAME: Cleaned node and bower modules\"
         npm install && bower install
         '" >> ~/.bash_profile
 
         source ~/.bash_profile
-        echo "=> Added clean and reinst aliases to .bash_profile"
+        echo "$PROGRAM_NAME: Added clean and reinst aliases to .bash_profile"
     else
-        echo "=> Files .bashrc and .bash_profile are missing, create one of them to add aliases"
+        echo "$PROGRAM_NAME: Files .bashrc and .bash_profile are missing, create one of them to add aliases"
     fi
 fi
